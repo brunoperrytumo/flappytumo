a765d421d0d321b0aee622b13039d1f537098c28
Bruno Perry <brunoperry@Brunos-Mac-Studio.local>
2026-02-05 12:47:20 +0000

    updated

--- game.js
+++ game.js
@@ -3,31 +3,37 @@ window.onload = async function () {
   let ctx = canvas.getContext("2d");
   ctx.imageSmoothingEnabled = false;
 
-  let birdBitmap;
+  let bird = {
+    x: 0,
+    y: 0,
+    w: 0,
+    h: 0,
+    bitmap: null,
+  };
 
   window.onresize = function () {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
+    const w = document.body.clientWidth;
+    const h = document.body.clientHeight;
 
-    console.log(document.body.clientWidth);
+    console.log(w, h);
     canvas.width = w < 540 ? w : 540;
     canvas.height = h < 900 ? h : 900;
-
-    setTimeout(() => {
-      console.log("w", window.innerWidth);
-    }, 1000);
   };
 
   async function loadData() {
     const req = await fetch("assets/flappy_bird.png");
     const blob = await req.blob();
-    const img = new Image();
 
-    birdBitmap = await createImageBitmap(blob);
+    bird.bitmap = await createImageBitmap(blob);
+    bird.w = bird.bitmap.width;
+    bird.h = bird.bitmap.height;
   }
-  function update() {}
+  function update() {
+    bird.y += 1;
+  }
   function draw() {
-    ctx.drawImage(birdBitmap, 0, 0, birdBitmap.width, birdBitmap.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(bird.bitmap, bird.x, bird.y, bird.w, bird.h);
   }
 
   function loop() {
