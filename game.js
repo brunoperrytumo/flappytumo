e57f7f32d21a82fcb4187725833995be0c812e80
Bruno Perry <brunoperry@Brunos-Mac-Studio.local>
2026-02-07 13:46:18 +0000

    update

--- game.js
+++ game.js
@@ -1,39 +1,17 @@
 window.onload = async function () {
-  let canvas = document.querySelector("canvas");
-  let ctx = canvas.getContext("2d");
-  ctx.imageSmoothingEnabled = false;
+  //MAIN MENU STUFF
+  let playButton;
+  let scoresButton;
+  function setupLayout() {
+    playButton = document.querySelector("#play-button");
+    scoresButton = document.querySelector("#scores-button");
 
-  let bird = {
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    bitmap: null,
-  };
-
-  window.onresize = function () {
-    const w = document.body.clientWidth;
-    const h = document.body.clientHeight;
-
-    canvas.width = w < 540 ? w : 540;
-    canvas.height = h < 900 ? h : 900;
-  };
-
-  async function loadData() {
-    const req = await fetch("assets/tumo_bird.png");
-    const blob = await req.blob();
-
-    bird.bitmap = await createImageBitmap(blob);
-    bird.width = 64;
-    bird.height = 64;
+    playButton.onclick = function () {
+      console.log("click");
+    };
   }
-  function update() {
-    bird.y += 1;
-  }
-  function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(bird.bitmap, bird.x, bird.y, bird.width, bird.height);
-  }
+  function update() {}
+  function draw() {}
 
   function loop() {
     update();
@@ -41,7 +19,6 @@ window.onload = async function () {
     requestAnimationFrame(loop);
   }
 
-  await loadData();
-  window.onresize();
+  setupLayout();
   loop();
 };
